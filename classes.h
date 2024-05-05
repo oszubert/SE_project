1ec976113b473349dd3f05150b6b416b9e13918e
oszubert <162124159+oszubert@users.noreply.github.com>
2024-05-06 00:02:04 +0200

    Usprawnienie rzutowania
    Zastąpienie obecnego sposobu rzutowania sposobem wykorzystującym cyfrowy analizator różnicowy (DDA - digital differential analyzer). Usunięcie niewykorzystywanego kodu (który był wykorzystywany pomocniczo przez poprzedni sposób rzutowania).

--- classes.h
+++ classes.h
@@ -17,15 +17,15 @@
 
 using namespace std;
 
-//const float M_PI=3.14159265358979323846;
+const float M_PI=3.14159265358979323846;
 
 const float windowWidth=1152.0f; // Szerokosc okna programu
 const float windowHeight=864.0f; // Wysokosc okna programu
 
 const float playerFOV=65.0f; // Pole widzenia gracza
 const unsigned int maxRayDepth=64; // Maksymalna glebokosc promienia
-const unsigned int rayCount=600; // Zmienna do pomocnicza do rzutowania
-const float columnWidth=windowWidth/(float)rayCount; // Zmienna pomocnicza do rzutowania
+//const unsigned int rayCount=600; // Zmienna do pomocnicza do rzutowania
+//const float columnWidth=windowWidth/(float)rayCount; // Zmienna pomocnicza do rzutowania
 
 const float playerSize=8.0f; // Rozmiar gracza
 const float moveSpeed=115.0f; // Predkosc ruchu gracza
@@ -103,7 +103,7 @@ public:
 
         float rad=angle*M_PI/180.0f;
 
-        float mouseSens = 50;
+        float mouseSens = 12.5;
         //angle += mouse.getPosition().x * gameTime * mouseSens;
         angle += (mousePos.x - windowWidth/2) * mouseSens * gameTime;
 
@@ -142,123 +142,15 @@ public:
 };
 
 
-
-struct Ray{ // Struktura promieni
-    sf::Vector2f drawPos;
-    sf::Vector2u mapPos;
-    float dist;
-    bool drawn;
-    bool drawnVertical;
-};
-
-static Ray rayCast(sf::Vector2f start, float angleDeg, const Map &map){ // Rzucanie promieni
-	float angle=angleDeg*M_PI/180.0f;
-	float vert_tan=-tan(angle), hor_tan=-1.0f/tan(angle);
-	float blockSize=map.getBlockSize();
-
-	bool drawn=false;
-	unsigned int vert_dof=0, hor_dof=0;
-	float vert_dist=numeric_limits<float>::max();
-	float hor_dist=numeric_limits<float>::max();
-//	float vert_dist=FLOAT_MAX;
-//	float hor_dist=FLOAT_MAX;
-
-    sf::Vector2u vert_mapPos, hor_mapPos;
-	sf::Vector2f vert_rayPos, hor_rayPos, offset;
-	if (cos(angle)>0.001f){
-		vert_rayPos.x=floor(start.x/blockSize)*blockSize+blockSize;
-		vert_rayPos.y=(start.x-vert_rayPos.x)*vert_tan+start.y;
-
-		offset.x=blockSize;
-		offset.y=-offset.x*vert_tan;
-	}
-	else if (cos(angle)<-0.001f){
-		vert_rayPos.x=floor(start.x/blockSize)*blockSize-0.01f;
-		vert_rayPos.y=(start.x-vert_rayPos.x)*vert_tan+start.y;
-
-		offset.x=-blockSize;
-		offset.y=-offset.x*vert_tan;
-	}
-	else vert_dof=maxRayDepth;
-
-	const auto &level=map.getLevel();
-
-	for (; vert_dof<maxRayDepth; vert_dof++){
-		int mapX=(int)(vert_rayPos.x/blockSize);
-		int mapY=(int)(vert_rayPos.y/blockSize);
-
-		if (mapY<level.size() && mapX<level[mapY].size() && level[mapY][mapX]!=sf::Color::Black){
-			drawn=true;
-			vert_dist=sqrt(
-                (vert_rayPos.x-start.x)*(vert_rayPos.x-start.x) +
-                (vert_rayPos.y-start.y)*(vert_rayPos.y-start.y)
-            );
-			vert_mapPos=sf::Vector2u(mapX, mapY);
-			break;
-		}
-
-		vert_rayPos+=offset;
-	}
-
-	if(sin(angle)>0.001f){
-		hor_rayPos.y=floor(start.y/blockSize)*blockSize+blockSize;
-		hor_rayPos.x=(start.y-hor_rayPos.y)*hor_tan+start.x;
-
-		offset.y=blockSize;
-		offset.x=-offset.y*hor_tan;
-	}
-	else if (sin(angle)<-0.001f){
-		hor_rayPos.y=floor(start.y/blockSize)*blockSize-0.01f;
-		hor_rayPos.x=(start.y-hor_rayPos.y)*hor_tan+start.x;
-
-		offset.y=-blockSize;
-		offset.x=-offset.y*hor_tan;
-	}
-	else hor_dof=maxRayDepth;
-
-	for (; hor_dof<maxRayDepth; hor_dof++){
-		int mapX=(int)(hor_rayPos.x/blockSize);
-		int mapY=(int)(hor_rayPos.y/blockSize);
-
-		if (mapY<level.size() && mapX<level[mapY].size() && level[mapY][mapX]!=sf::Color::Black){
-			drawn=true;
-			hor_dist=sqrt(
-                (hor_rayPos.x-start.x)*(hor_rayPos.x-start.x)+
-                (hor_rayPos.y-start.y)*(hor_rayPos.y-start.y)
-            );
-			hor_mapPos=sf::Vector2u(mapX, mapY);
-			break;
-		}
-
-		hor_rayPos+=offset;
-	}
-
-	return Ray{hor_dist<vert_dist ? hor_rayPos : vert_rayPos, hor_dist < vert_dist ? hor_mapPos : vert_mapPos, min(hor_dist, vert_dist), drawn, vert_dist <= hor_dist};
-}
-
 class RayRender{ // Rysowanie promieni
 private:
     sf::Texture wallTex;
-    sf::Sprite wallSprite;
 public:
     void init(){ // Tworzenie tekstury i sprite
         if (!wallTex.loadFromFile("brick.png")){
             cerr<<"Nie ma pliku brick.png"; return;}
         if(wallTex.getSize().x != wallTex.getSize().y){
             cerr<<"Tekstura nie jest kwadratem"; return;}
-        wallSprite = sf::Sprite(wallTex);
-    }
-    void drawRays(sf::RenderTarget &target, const Player &player, const Map &map){ // Rysowanie promieni
-        for(float angle=player.angle-playerFOV/2.0f; angle<player.angle+playerFOV; angle+=0.5f){
-            Ray ray=rayCast(player.pos, angle, map);
-
-            if(ray.drawn){
-                sf::Vertex line[]={sf::Vertex(player.pos), sf::Vertex(ray.drawPos),};
-                line[0].color=sf::Color::Cyan;
-                line[1].color=sf::Color::Cyan;
-                target.draw(line, 2, sf::Lines);
-            }
-        }
     }
 
     void render3D(sf::RenderTarget &target, const Player &player, const Map &map){ // Rysowanie w 3D (tymczasowe, zostanie zastapione algorytmem Digital Differential Analysis
@@ -270,67 +162,86 @@ public:
         rectangle.setFillColor(sf::Color(14, 176, 92)); // Kolor ziemii
         target.draw(rectangle);
 
-        const sf::Color fogColor=sf::Color(81, 187, 254);
-        const float maxRenderDist=maxRayDepth*map.getBlockSize();
-        const float maxFogDist=maxRenderDist/4.0f;
+        float rad=player.angle*M_PI/180.0f;
+        sf::Vector2f direction{cos(rad), sin(rad)};
+        sf::Vector2f plane{-direction.y, direction.x * 0.66f};
 
-        sf::RectangleShape column{sf::Vector2f(1.0f,1.0f)};
-        float angle=player.angle-playerFOV/2.0f;
-        float angleMove=playerFOV/(float)rayCount;
-        for(unsigned int i=0; i<rayCount; i++, angle+=angleMove){
-            Ray ray=rayCast(player.pos, angle, map);
+        sf::VertexArray walls{sf::Lines};
+        for(unsigned int i=0; i<windowWidth; i++){
+            float cameraX=i*2.0f/windowWidth-1.0f;
+            sf::Vector2f rayPos=player.pos/map.getBlockSize();
+            sf::Vector2f rayDir=direction+plane*cameraX;
 
-            if(ray.drawn){
-                ray.dist*=cos((player.angle-angle)*M_PI/180.0f);
+            sf::Vector2f deltaDist{
+                abs(1.0f/rayDir.x),
+                abs(1.0f/rayDir.y),
+            };
 
-                float wallHeight=(map.getBlockSize()*windowHeight)/ray.dist;
-                float wallOffset=windowHeight/2.0f-wallHeight/2.0f;
+            sf::Vector2i mapPos{rayPos};
+            sf::Vector2i step;
+            sf::Vector2f sideDist;
 
-                float textureDiv;
-                if(ray.drawnVertical){
-                    textureDiv = ray.drawPos.y -
-                                 wallTex.getSize().x * floor(ray.drawPos.y / wallTex.getSize().x);
+            if (rayDir.x<0.0f){
+                step.x=-1;
+                sideDist.x=(-mapPos.x+rayPos.x)*deltaDist.x;
+            }
+            else{
+                step.x=1;
+                sideDist.x=(mapPos.x-rayPos.x+1.0f)*deltaDist.x;
+            }
+
+            if(rayDir.y<0.0f){
+                step.y=-1;
+                sideDist.y=(-mapPos.y+rayPos.y)*deltaDist.y;
+            }
+            else{
+                step.y=1;
+                sideDist.y=(mapPos.y-rayPos.y+1.0f)*deltaDist.y;
+            }
+            bool rayHit{}, hitVertical{};
+
+            unsigned int depth=0;
+            while(!rayHit && depth<maxRayDepth){
+                if(sideDist.x<sideDist.y){
+                    sideDist.x+=deltaDist.x;
+                    mapPos.x+=step.x;
+                    hitVertical=false;
                 }
                 else{
-                    textureDiv = wallTex.getSize().x * ceil(ray.drawPos.x / wallTex.getSize().x)
-                                 - ray.drawPos.x;
+                    sideDist.y+=deltaDist.y;
+                    mapPos.y+=step.y;
+                    hitVertical=true;
                 }
 
-                wallSprite.setPosition(i*columnWidth, wallOffset);
-                wallSprite.setTextureRect(sf::IntRect(textureDiv,0,
-//                                                      wallTex.getSize().x / map.getBlockSize(),
-                                                      columnWidth, // ?? Z poprzednia linijka tekstury zle "podchodzily" pod mgle
-                                                      wallTex.getSize().y));
-                wallSprite.setScale(columnWidth, wallHeight / wallTex.getSize().y);
-
-                if(wallHeight>windowHeight)wallHeight=windowHeight;
-
-                float brightness=1.0f-(ray.dist/maxRenderDist);
-                if(brightness<0.0f) brightness=0.0f;
-
-                float shade=(ray.drawnVertical ? 0.8f : 1.0f)*brightness;
-
-
-                float fogVisibility = (pow(ray.dist/maxFogDist, 2)); //krzywa mgly
-                if(fogVisibility>1.0f) fogVisibility=1.0f;
-//
-                column.setPosition(i*columnWidth, wallOffset);
-                column.setScale(columnWidth, wallHeight);
-                column.setFillColor(sf::Color(fogColor.r, fogColor.g, fogColor.b, fogVisibility * 255));
-//                sf::Color color=map.getLevel()[ray.mapPos.y][ray.mapPos.x];
-//                color=sf::Color(color.r*shade, color.g*shade, color.b*shade);
-//                column.setFillColor(sf::Color(
-//                    (1.0f-fogVisibility)*color.r+fogVisibility*fogColor.r,
-//                    (1.0f-fogVisibility)*color.g+fogVisibility*fogColor.g,
-//                    (1.0f-fogVisibility)*color.b+fogVisibility*fogColor.b)
-//                );
-                wallSprite.setColor(sf::Color(255*shade, 255*shade, 255*shade));
-                target.draw(wallSprite);
-                target.draw(column);
-//                cout<<wallTex.getSize().x<<" "<<columnWidth<<endl;
-
+                int x=mapPos.x, y=mapPos.y;
+                const auto &level=map.getLevel();
+                if (y>=0 && y<level.size() && x>=0 && x<level[y].size() && level[y][x]!=sf::Color::Black) rayHit=true;
+                depth++;
             }
+
+            if(rayHit){
+                float perpWallDist=hitVertical ? sideDist.y-deltaDist.y : sideDist.x-deltaDist.x;
+                float wallHeight=windowHeight/perpWallDist;
+                float wallStart=(-wallHeight+windowHeight)/2.0f;
+                float wallEnd=(wallHeight+windowHeight)/2.0f;
+                float textureSize=wallTex.getSize().x;
+
+                float wallX=hitVertical ? rayPos.x+perpWallDist*rayDir.x : rayPos.y+perpWallDist*rayDir.y;
+                wallX-=floor(wallX);
+                float textureX=wallX*textureSize;
+
+                float brightness=1.0f-(perpWallDist/(float)maxRayDepth);
+                if(hitVertical) brightness*=0.7f;
+
+                sf::Color color=sf::Color(255*brightness, 255*brightness, 255*brightness);
+
+                walls.append(sf::Vertex(sf::Vector2f((float)i, wallStart), color, sf::Vector2f(textureX, 0.0f)));
+                walls.append(sf::Vertex(sf::Vector2f((float)i, wallEnd), color, sf::Vector2f(textureX, textureSize)));
+            }
+
         }
+        sf::RenderStates states{&wallTex};
+        target.draw(walls, states);
     }
 };
 
